9bc76aa3de7f9a4c61eb70e535a5ac8434cc4057
Shipulin1990 <168572651+Shipulin1990@users.noreply.github.com>
2024-06-15 18:15:39 +0300

    Add files via upload

---
+++ homework2-6.py
@@ -0,0 +1,14 @@
+def single_root_words(root_word, *other_words):
+    same_words = []
+    for word in other_words:
+        if root_word.startswith(word) or word.startswith(root_word):
+            same_words.append(word)
+    return same_words
+
+
+print(single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies'))
+# Ответ: ['richiest', 'richies']
+
+
+print(single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel'))
+# Ответ: ['Disable']

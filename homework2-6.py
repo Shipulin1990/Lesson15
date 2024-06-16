4d85f1703694080c915f2e7c0d47c82eced5a8c7
Shipulin1990 <168572651+Shipulin1990@users.noreply.github.com>
2024-06-16 21:55:11 +0300

    Update homework2-6.py

--- homework2-6.py
+++ homework2-6.py
@@ -5,10 +5,23 @@ def single_root_words(root_word, *other_words):
             same_words.append(word)
     return same_words
 
-
+print('Вариант 1')
 print(single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies'))
 # Ответ: ['richiest', 'richies']
 
+print(single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel'))
+# Ответ: ['Disable']
+
+def single_root_words(root_word, *other_words):
+    same_words = []
+    for word in other_words:
+        if root_word.lower().count(word.lower()) or word.lower().count(root_word.lower()):
+            same_words.append(word)
+    return same_words
+
+print('Вариант 2')
+print(single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies'))
+# Ответ: ['richiest', 'richies']
 
 print(single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel'))
 # Ответ: ['Disable']
